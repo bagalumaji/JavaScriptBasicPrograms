cef7e68cf9ff2e3c02d67b2eae61ed106abe1f1d
bagalumaji <bagalumaji@gmail.com>
2022-07-17 20:02:17 +0530

    created basic demo files

---
+++ datatypes.js
@@ -0,0 +1,18 @@
+/*
+
+ 1. Number 
+ 2. String
+ 3. boolean
+*/
+
+let a=10
+let b=2.2;
+let c='hello'
+let d=true;
+let e = 'a';
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
---
+++ variables.js
@@ -0,0 +1,14 @@
+// var older way
+//let and const
+
+let num1=10;
+
+console.log('number 1 : '+num1);
+num1 = 20;
+console.log('updated number1 : '+num1);
+
+const num2 = 100 ;
+
+console.log("constant value  : "+num2);
+
+//num2 =  300;
