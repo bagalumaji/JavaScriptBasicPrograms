3863f6c398f281a0c4e7806d159bd7c63276bf5c
bagalumaji <bagalumaji@gmail.com>
2022-07-17 20:53:01 +0530

    added oper and extra file

---
+++ basicprograms/datatypes.js
@@ -0,0 +1,23 @@
+/*
+
+ 1. Number 
+ 2. String
+ 3. boolean
+ 4. object
+ 5. Bigint
+ 6. null
+ 7. undefined
+ 8. symbol
+*/
+
+let a=10
+let b=2.2;
+let c='hello'
+let d=true;
+let e = 'a';
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
---
+++ oper/arithmetic.js
@@ -0,0 +1,26 @@
+let num1='3';
+let num2=3;
+
+let result = num1 + num2;
+
+console.log(result);
+
+ result = 3 + 33 + 300+'sayaji'
+
+ console.log(result);
+
+
+ result = 'sharu'+ 3 + 33+ 330;
+ console.log(result);
+
+ let x1 = '3' - true;
+ console.log(x1);
+
+ let x2 = '4' + true;
+
+ console.log('output ',x2);
+
+ console.log(2/0);
+ console.log(4%3);
+
+ 
