ed3f57cbbca7d6a3813c0d937ce5a67cbe32d3de
bagalumaji <bagalumaji@gmail.com>
2023-11-05 22:23:51 +0530

    22. Add an element to an array.

---
+++ arrays/addelement2array.js
@@ -0,0 +1,11 @@
+//Add an element to an array.
+
+const arr = [1, 2, 3, 4, 5];
+console.log("before adding element of array ", arr);
+
+// it will add at end of array
+arr.push(6);
+console.log("after adding element of array ", arr);
+
+arr.unshift(7);
+console.log("after adding element of array ", arr);
