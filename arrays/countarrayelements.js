db11d719ac87e48f2323806a753bea93dae80931
bagalumaji <bagalumaji@gmail.com>
2023-11-05 22:25:21 +0530

    Count the number of elements in an array

---
+++ arrays/countarrayelements.js
@@ -0,0 +1,11 @@
+//Count the number of elements in an array
+
+const arr = [1, 2, 3, 4, 5];
+
+let count = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  count++;
+}
+
+console.log("Count: " + count);
