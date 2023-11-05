71182b319dea83696a9af7a5464259fcad329453
bagalumaji <bagalumaji@gmail.com>
2023-11-05 22:28:31 +0530

    21. Delete an element from an array.

---
+++ arrays/deletelementfromarray.js
@@ -0,0 +1,11 @@
+//21. Delete an element from an array.
+
+const arr = [1, 2, 3, 4, 5];
+
+let ele = arr.pop();
+console.log("deleted element : ", ele);
+console.log("array element : ", arr);
+
+ele = arr.shift();
+console.log("deleted element : ", ele);
+console.log("array element : ", arr);
